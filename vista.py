dc7c7d1380327f255bdeeb34c3c83f3ecc3f2816
Axel Alán Martínez Tamez <60474869+ShadowBolt09@users.noreply.github.com>
2023-11-24 23:04:39 -0600

    y

--- vista.py
+++ vista.py
@@ -173,6 +173,95 @@ def borrar():
     cursor.close()
     return ver_datos()
 
+'''
+# Verificamos el inicio de sesión mediante Ajax
+@app.route('/login', methods=['GET','POST'])
+def login():
+    try:
+        email = request.form['email'].strip() # quitamos espacios en blanco
+        contrasena = request.form['contrasena'].strip()
+        print(f"Intento de inicio de sesión. Email: {email}, Contraseña: {contrasena}")
+
+        # Llamada al procedimiento almacenado
+        cur = mysql.connection.cursor()
+        cur.callproc('verificar_login', (email, contrasena))
+        resultado = cur.fetchone()
+        cur.close()
+
+        print(f"Email: {email}, Contraseña: {contrasena}, Resultado: {resultado}")
+
+        if resultado is not None and len(resultado) > 0:
+            resultado = resultado[0]
+
+        ## como sigue diciendo que el login es incorrecto,
+        ## usemos valores booleanos
+            if resultado:
+                flash('Inicio de sesión exitoso', 'success')
+                response = jsonify({'resultado': 'Inicio de sesión exitoso'})
+
+                # Verificar si es una solicitud AJAX, Redirigir solo en solicitudes AJAX
+                if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                    return response  # Respuesta JSON para solicitudes AJAX
+                else:
+                    print("Redirigiendo a home")
+                    return redirect(url_for('home'))
+            else:
+                flash('Credenciales incorrectas', 'error')
+                return jsonify({'error': 'Credenciales incorrectas'})
+        else:
+            return jsonify({'error': 'Credenciales incorrectas'})
+
+    except Exception as e:
+        print(f"Error durante el inicio de sesión: {str(e)}")
+        traceback.print_exc()  # Imprime la info completa
+
+        return jsonify({'error': 'Hubo un error durante el inicio de sesión'})
+
+    return jsonify({'resultado': 'Éxito'})
+
+# Ruta de registro
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+# Realizamos el registro mediante Ajax igual que el login
+@app.route('/registrar_usuario', methods=['POST'])
+def registrar_usuario():
+    try:
+        # Obtener datos del formulario
+        nombre = request.form.get('nombre')
+        appaterno = request.form.get('appaterno')
+        apmaterno = request.form.get('apmaterno')
+        correo = request.form.get('correo')
+        contrasena = request.form.get('contrasena')
+
+        # Validar que todos los campos necesarios estén presentes
+        if not nombre or not appaterno or not apmaterno or not correo or not contrasena:
+            return jsonify({'error': 'Todos los campos son obligatorios'})
+
+        # Llamada al procedimiento almacenado
+        cur = mysql.connection.cursor()
+        cur.callproc('registroUsuario', (nombre, appaterno, apmaterno, correo, contrasena))
+        resultado = cur.fetchone()
+        print(f"Después de llamar al procedimiento almacenado. Resultado: {resultado}") 
+
+        # Verificar si se obtuvieron resultados
+        if resultado is not None and len(resultado) > 0:
+            mensaje = resultado[0]
+            return jsonify({'mensaje': mensaje})
+        else:
+            return jsonify({'error': 'No se obtuvo un mensaje del servidor'})
+
+    except Exception as e:
+        print(f"Error durante el registro: {str(e)}")
+        return jsonify({'error': 'Hubo un error durante el registro'})
+    finally:
+        # Asegurarse de cerrar el cursor
+        if cur:
+            cur.close()
+            # Hacer commit para confirmar los cambios
+            mysql.connection.commit()
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
